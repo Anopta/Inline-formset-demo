c96afc18ac7c73af3087721ef7132a0510b6acca
Anopta <66787374+Anopta@users.noreply.github.com>
2022-07-19 14:08:52 +0900

    created env

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,9 +1,17 @@
-from ajax_select.admin import AjaxSelectAdmin
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product, DeliveryPoint, ProductType
+from .models import *
 
-admin.site.register(Product)
-admin.site.register(DeliveryPoint)
+class DeliveryInline(admin.StackedInline):
+    '''Stacked Inline View for '''
+    model = DeliveryPoint
+    min_num = 0
+    max_num = 3
+    extra = 1
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ DeliveryInline]
+
 admin.site.register(ProductType)
